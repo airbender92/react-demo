102ee889143ea3423dd7294dd7323ea7eccc2bcf
wangyunbo <wangyunbo@hatech.com.cn>
2022-10-27 17:54:51 +0800

    update

--- packages/react-admin/src/Admin.tsx
+++ packages/react-admin/src/Admin.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { ComponentType } from 'react';
+import { CoreAdminProps, localStorageStore } from '@react-demo/ra-core'
+import { AdminUI, AdminContext } from '@react-demo/ra-ui-materialui'
+import { } from '@mui/material';
 
+import { defaultI18nProvider } from './defaultI18nProvider'
 export const Admin = (props: any) => {
     return <div>react-admin</div>;
 };
